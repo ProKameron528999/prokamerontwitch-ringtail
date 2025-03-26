8abd5fa6f63404d8ee9fc13608b8fd7252343794
Glitch (glitch-hello-node) <none>
2025-03-26 22:59:49 +0000

    👩‍👩‍👦🍓 Checkpoint ./server.js:2436677/1505

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.exit()
+//process.exit()
 
 const tmi = require('tmi.js');
 
@@ -27,10 +27,17 @@ const client = new tmi.Client({
 client.on('message', (channel, tags, message, self) => {
     if (self) return; // Ignore messages from the bot itself
     console.log(`[${tags['display-name']}]: ${message}`);
-    
+   // console.log(tags['display-name'])
     // Respond if the message contains "hello"
     if (message.includes('Epic moenys have been distributed. Time to gamble! LETSGO') || message.includes("@ringtail216 received")) {
+      if(tags['display-name'] !== "ringbot216") {
         client.say(channel, '!takepoints');
+      } else
+      {client.say(channel, `dude I'm not stupid ${tags['display-name']}`)}
+    }
+    if (message.includes('who am i')) {
+        client.say(channel, `your name is ${tags['display-name']}`);
+  
     }
 });
 
