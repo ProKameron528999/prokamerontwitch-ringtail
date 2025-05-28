b7915ae118f436be4af0553cc55045b3a66f77b0
Glitch (prokamerontwitch-ringtail) <none>
2025-05-28 02:05:24 +0000

    🌨🌿 Checkpoint ./server.js:2436677/72

--- server.js
+++ server.js
@@ -1160,8 +1160,8 @@ io.on("connection", (socket) => {
     wheelBlacklisted.clear();
     wheelAccepting = true;
     io.emit("resetWheel");
-        io.emit("wheelAdd", "Codename: UB_Developer");
-    wheelEntries.push("Codename: UB_Developer");
+        io.emit("wheelAdd", "prokameron");
+    wheelEntries.push("prokameron");
   });
 });
 
